5cfcab9a387aa2a80dd12265693b177a158506c0
a <justinc1127@nycstudents.net>
2025-09-18 10:16:13 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-x = 3
-y = float(3)
-print(x,y)
-bill=x
-tip=x*0.18
+bill= input("How much is the bill?")
+bill=float(bill)
+
+
+tip=bill*0.18
+
 total= tip + bill
+print(str("Your total is ")+ str(total))
