0084a8c341f0b611c46662e1305178811eb27474
a <justinc1127@nycstudents.net>
2025-09-19 09:54:33 -0400

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,14 @@
 # total= tip + bill
 # print(str("Your total is ")+ str(total))
 
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+
+# print(values[0])
+# print(values[6])
+
+x = "this is a thing"
+y= x.split()
+z = y[0]
+print(y)
+print(z)
