5d0890d0e173e36588c2a6ec1298860dfb66cb7a
a <justinc1127@nycstudents.net>
2025-09-19 09:29:14 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-bill= input("How much is the bill?")
-bill=float(bill)
+# bill= input("How much is the bill?")
+# bill=float(bill)
 
 
-tip=bill*0.18
+# tip=bill*0.18
 
-total= tip + bill
-print(str("Your total is ")+ str(total))
+# total= tip + bill
+# print(str("Your total is ")+ str(total))
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
