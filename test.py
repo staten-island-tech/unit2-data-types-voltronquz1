871e8b93ce6967b571187edd115d869114c6c78b
a <justinc1127@nycstudents.net>
2025-09-19 10:04:17 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import random
+
 # bill= input("How much is the bill?")
 # bill=float(bill)
 
@@ -13,8 +15,17 @@
 # print(values[0])
 # print(values[6])
 
-x = "this is a thing"
-y= x.split()
-z = y[0]
+
+# x=input("Do you want to buy  thing? (y/n)")
+
+# while x is not "n":
+#     item=input("what yu wanna buy")
+# print("thanks for purchase")
+
+y=random.randit(1,100)
 print(y)
-print(z)
+# x = "this is a thing"
+# y= x.split()
+# z = y[0]
+# print(y)
+# print(z)
