8eac0f534f710a1cf09a9c2879fc9db31c95cdb1
a <justinc1127@nycstudents.net>
2025-10-03 09:49:13 -0400

    Create tes.py

---
+++ tes.py
@@ -0,0 +1,8 @@
+# Assessment Pratice
+x=input(print("Give me a parkinglot with parking spaces"))
+for i in range(x):
+    
+
+# def language(x):
+#     print("do something")
+# language("Cadee computer never seems to work in the morning")
