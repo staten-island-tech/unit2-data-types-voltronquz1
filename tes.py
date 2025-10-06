01f0d612d98fa85e11e1812d4a9ab86a502e4e8f
a <justinc1127@nycstudents.net>
2025-10-06 09:58:53 -0400

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,9 +1,19 @@
 # Assessment Pratice
-def lang(x):
+# def lang(x):
     # print(x[0])
-    for char in x:
-        print(char)
+    # for char in x:
+    #     print(char)
 
 # def language(x):
 #     print("do something")
-lang("Cadee computer never seems to work in the morning")
+# lang("Cadee computer never seems to work in the morning")
+
+
+char1="t"
+char2="s"
+def lang(x):
+    if char1 > char2:
+        print("English")
+    elif char1 < char2:
+        print("French")
+lang("")
