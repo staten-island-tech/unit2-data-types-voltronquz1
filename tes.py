d8098d47c58e0a864e93603136d2c619c48534ef
a <justinc1127@nycstudents.net>
2025-10-08 10:16:40 -0400

    Update tes.py

--- tes.py
+++ tes.py
@@ -25,23 +25,30 @@
 # lang("Lorsque j'avais six ans j'ai vu, une fois,")
 
 
-def spaces(x,y):
-    c=0
-    e=0
-    for car in x:
-        if car=="C":
-            c+=1
-        elif car==".":
-            e+=1
-    C=0
-    E=0
-    for cars in y:
-        if cars=="C":
-            C+=1
-        elif cars==".":
-            E+=1
-            
+def spaces(n,y,t):
+    occupied=0
+    for i in range(n):
+        print(y[i],t[i])
+        if y[i]=="C" and t[i]=="C":
+            occupied+=1
+    return(occupied)
+        
 
 
-spaces("CCC..","C.C.C.")
+print(spaces(5,"CCC..","C.C.C."))
 
+# def spaces(x,y):
+#     c=0
+#     e=0
+#     for car in x:
+#         if car=="C":
+#             c+=1
+#         elif car==".":
+#             e+=1
+#     C=0
+#     E=0
+#     for cars in y:
+#         if cars=="C":
+#             C+=1
+#         elif cars==".":
+#             E+=1
