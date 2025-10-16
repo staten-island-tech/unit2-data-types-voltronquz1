f2ccccb6f2383b725ced2879971d6ebf6d868e17
a <justinc1127@nycstudents.net>
2025-10-15 20:05:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -101,37 +101,22 @@ import random
 #     if x%i==0:
 #         factors.append(i)
 # print(f"The factors of {x} is {factors}")
-# GCF CHALLENGE 4
-# num1=input("give me a number")
-# num2=input("give me anothe number")
-# cf=[]
-# fac1=[]
-# fac2=[]
-# def gcf(x,y): 
-#     for i in range(1,num1+1):
-#         if num1%i==0 and num2%i==0:
-#             gcf.append(i)
-# print(gcf)
+GCF CHALLENGE 4
+num1=input("give me a number")
+num2=input("give me anothe number")
+cf=[]
+fac1=[]
+fac2=[]
+def gcf(x,y): 
+    for i in range(1,num1+1):
+        if num1%i==0:
+            fac1.append(i)
+    for i in range(1,num2+1):
+        if num2%i==0:
+            fac2.append(i)
+print(gcf)
 
 
 
 
-def gcf():  #defines gcf as a function
-    number1 = int(input("Enter 1st number:"))
-    number2 = int(input("Enter 2nd number:"))
-    common_factors = []
-    list_of_factor1 = []        #list for store the factors
-    list_of_factor2 = []
-    for i in range(1, number1 + 1): #for anynumber/i between of i and number1, start at 1 and keep add 1 to number1
-        if number1 % i == 0:    #divide number1 + 1 by i and if remainder = 0 store fir list1
-            list_of_factor1.append(i)
-    for i in range(1, number2 + 1): #for anynumber/i in range of i and number2, starts at 1 keeps adding 1 to number2
-        if number2 % i == 0:    #divide number2 + 1 by i and if remainder = 0 store for list2
-            list_of_factor2.append(i)
-    for i in list_of_factor1:
-        if i in list_of_factor2:        #check for same numbers in both lists and founded same numbers get add to common_factors list
-            common_factors.append(i)
-    gcf = max(common_factors)   #max find biggest number in common_factors list and define it as variable gcf
-    print(f"The GCF of {number1} and {number2} is {gcf}")
 
-gcf()
